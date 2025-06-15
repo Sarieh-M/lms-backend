952c7bc4f935225e9f51885cca370a1cb3eef2b5
Noor ALdeen <mhabnoor75@gmail.com>
2025-06-15 20:25:27 +0300

    this one for render

--- src/user/dto/login.dto.ts
+++ src/user/dto/login.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsString, Length, Matches, MaxLength } from "class-validator";
+import { IsNotEmpty, IsString, Length, Matches, MaxLength } from "class-validator";
 
 
 export class LoginDto{
